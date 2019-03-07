4f6626ebf83916b945b9fa9404960b5276ed71d5
kageanrage <kevinjpickett@gmail.com>
2019-03-07 23:42:41 +1100

    added start and end date calculation function

--- create.py
+++ create.py
@@ -3,6 +3,9 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from config import Config   # this imports the config file where the private data sits
 import pandas as pd
+import datetime
+import calendar
+from dateutil.relativedelta import *
 
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')  # turns on logging
 # logging.disable(logging.CRITICAL)     # switches off logging when desired
@@ -10,33 +13,42 @@ logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - 
 cfg = Config()  # create an instance of the Config class, essentially brings private config data into play
 os.chdir(cfg.cwd)  # change the current working directory to the one stipulated in config file
 
-logging.debug('Imported modules')
-logging.debug('Start of program')
-logging.debug(f'Current cwd = {os.getcwd()}')
+# logging.debug('Imported modules')
+# logging.debug('Start of program')
+# logging.debug(f'Current cwd = {os.getcwd()}')
 
 p_number_to_search = 'P-46240'
 date_example = "07/03/2019 00:00:00"
 # TODO: code to create Start Date (today's date) and End Date (last day of next month)
 
+
+def generate_dates():
+    now = datetime.datetime.now()  # current date and time as datetime object
+    start_date_string = now.strftime("%d/%m/%Y %H:%M:%S")  # current date and time as string
+    # print(f'Start Date is {start_date_string}')
+    this_time_next_month = now + relativedelta(months=+1)  # date time object for today's date + time in a month
+    next_month = this_time_next_month.month  # isolating just the number of next month
+    year_next_month = this_time_next_month.year  # isolating just the year it will be next month
+    next_month_string = str(next_month)  # convert number of next month to a string
+    if len(next_month_string) == 1:  # if next month is only 1 digit...
+        next_month_string = "0" + next_month_string  # ...add a leading zero
+    end_date_string = str(calendar.monthrange(year_next_month, next_month)[1]) + "/" + str(next_month_string) \
+                      + "/" + str(year_next_month) + " 00:00:00"  # compile full end date string
+    return start_date_string, end_date_string
+
+
+start_date, end_date = generate_dates()  # generates start and end dates through the function
+
 qf_msg = cfg.qf_msg
 so_msg = cfg.so_msg
 comp_msg = cfg.comp_msg
-
 
 survey_name_col = 'Survey Name'
 topic_col = 'Topic'
 p_number_col = 'SE Project Number'
 expected_loi_col = 'Expected LOI'
 client_name_col = 'Client name'
-
-
-
-
-
-
 sales_contact_col = 'Sales Contact'
-
-
 edge_credits_col = 'Edge Credits'
 
 
