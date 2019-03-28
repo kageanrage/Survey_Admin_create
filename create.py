f4766cc1e091e26bd5ac1bc7092e5f503a2f15e4
kageanrage <kevinjpickett@gmail.com>
2019-03-28 12:32:41 +1100

    fixed close date generator bug, testing API implementation

--- create.py
+++ create.py
@@ -58,29 +58,36 @@ def generate_dates_sa():
 
 
 def generate_closing_date():
-    close_month_trimmed = close_month_raw[0:10]
-    close_month = datetime.datetime.strptime(close_month_trimmed, '%Y-%m-%d')
-    last_day_in_close_month = calendar.monthrange(close_month.year, close_month.month)[1]
-    closing_date_string = str(last_day_in_close_month) + '/' + str(close_month.month) + '/' + str(close_month.year)
+    print(f"close_date_raw is {close_date_raw}")
+    close_date_trimmed = close_date_raw[0:10]
+    print(f"close_date_trimmed is {close_date_trimmed}")
+    close_date = datetime.datetime.strptime(close_date_trimmed, '%Y-%m-%d')
+    last_day_in_close_month = calendar.monthrange(close_date.year, close_date.month)[1]
+    close_month_string = str(close_date.month)  # convert number of close month to a string
+    print(f"close_month_string is {close_month_string}")
+    if len(close_month_string) == 1:  # if close month is only 1 digit...
+        close_month_string = "0" + close_month_string  # ...add a leading zero
+    print(f"After adding a leading zero where needed, close_month_string is now {close_month_string}")
+    closing_date_string = str(last_day_in_close_month) + '/' + close_month_string + '/' + str(close_date.year)
+    print(f"closing_date_string is {closing_date_string}")
     return closing_date_string
 
 
 def date_reshuffler(original_date):
+    logging.debug(f"original date is {original_date}")
     revised_date = original_date[6:10] + "-" + original_date[3:5] + "-" + original_date[0:2]
     logging.debug(f"revised date is {revised_date}")
     return revised_date
 
 
 # p_number = 'P-46262'  # this is hardcoded for testing - won't be needed once Zoho is in the chain
-# survey_name_to_search = 'Test KP'  # this is hardcoded for testing - won't be needed once argument passed in bat file
+survey_name_to_search = 'KP test 28-03-19'  # TEST MODE - this is hardcoded for testing - won't be needed once argument passed in bat file
 
-survey_name_to_search = pass_in_survey_name()  # grabs survey name from batch file as argument
-
+# survey_name_to_search = pass_in_survey_name()  # LIVE MODE - grabs survey name from batch file as argument
 
 start_date, end_date, proposal_date = generate_dates_sa()  # generates start and end dates through the function
 
-
-# define all variables here
+# define variables here
 qf_msg = cfg.qf_msg
 so_msg = cfg.so_msg
 comp_msg = cfg.comp_msg
@@ -88,7 +95,7 @@ comp_msg = cfg.comp_msg
 # varied for each project
 p_number_col = 'SE Project Number'
 
-# Project Tracking Sheet - columns of interest for SQL query
+# Project Tracking Sheet - column names of interest for SQL query
 survey_name_col = 'Survey Name'
 status = 'Active'
 topic_col = 'Topic'
@@ -105,7 +112,7 @@ qf_outcome_reward_id = 'Disqualified Survey - Regular Prize Draw'
 so_outcome_reward_id = 'Disqualified Survey - Regular Prize Draw'
 comp_outcome_reward_id = 'Completed Survey - Regular Prize Draw'
 comp_secondary_reward_type = 'Credits'
-tc_filepath = cfg.tc_filepath
+tc_filepath = cfg.tc_filepath  # file path of T&Cs pdf file
 
 excel_name_path = cfg.live_excel_file_path + "\\" + cfg.live_excel_filename  # LIVE MODE VERSION
 excel_file_name_path_ext = excel_name_path + ".xlsm"  # LIVE MODE VERSION
@@ -142,7 +149,7 @@ expected_loi = str(all_rows[0][1])
 client_name = str(all_rows[0][2])
 sales_contact = str(all_rows[0][3])
 edge_credits = str(int(all_rows[0][4]))
-close_month_raw = all_rows[0][5]
+close_date_raw = all_rows[0][5]
 
 
 # define fixed variables for Zoho
@@ -154,6 +161,135 @@ campaign_start_date = start_date[0:10]  # same as start date in survey admin, wh
 closing_date = generate_closing_date()
 campaign_end_date = closing_date  # same as closing date
 
+# TODO: update Zoho section so it uses API instead of GUI
+
+# 0 run this code every single time:
+zcrmsdk.ZCRMRestClient.initialize()
+
+# 2 - second chunk of code (run in isolation) - I ran this to attempt to generate 'access token through refresh token' i.e. add it to the token file
+oauth_client = zcrmsdk.ZohoOAuth.get_client_instance()
+refresh_token = cfg.refresh_token
+user_identifier = cfg.zoho_uname
+oauth_tokens = oauth_client.generate_access_token_from_refresh_token(refresh_token, user_identifier)
+
+# create format-adjusted date variables for use by API
+proposal_date_api = date_reshuffler(proposal_date)
+closing_date_api = date_reshuffler(closing_date)
+campaign_start_date_api = date_reshuffler(campaign_start_date)
+campaign_end_date_api = date_reshuffler(campaign_end_date)
+
+# 3 - if access token already refreshed in past hour, can proceed without any initialisation code apart from what's specified at '# 0'
+
+
+def search_contact_by_name(keyword):
+    logging.debug('Function: search_contact_by_name')
+    try:
+        module_ins = ZCRMModule.get_instance('Contacts')  # module API Name
+        resp = module_ins.search_records(keyword)  # search key word
+        # print(resp.status_code)
+        resp_info = resp.info
+        # print('resp_info looks like this:')
+        # print(resp_info)
+        record_ins_arr = resp.data
+        # print('record_ins_array looks like this:')
+        # print(record_ins_arr)
+        logging.debug(f'Number of contacts found with this name (i.e. len of record_ins_arr) is {len(record_ins_arr)}')
+        first_record = record_ins_arr[0]
+        contact_id = first_record.entity_id
+
+        record_ins_item_1_data = record_ins_arr[0].field_data
+        # print('record looks like this:')
+        # pprint(record_ins_item_1_data)
+        full_name = record_ins_item_1_data['Full_Name']
+
+        logging.debug(f"Returning Contact ID corresponding to {full_name}")
+        logging.debug(f"ID found: {contact_id}")
+        return contact_id
+
+    except zcrmsdk.ZCRMException as ex:
+        print(ex.status_code)
+        print(ex.error_message)
+        print(ex.error_code)
+        print(ex.error_details)
+        print(ex.error_content)
+
+
+def create_potential():
+    logging.debug('Function: create_potential')
+    try:
+        record = ZCRMRecord.get_instance('Potentials')  # module API Name
+
+        record.set_field_value('Account_Name', client_name)
+        record.set_field_value('Deal_Name', survey_name)
+        record.set_field_value('Industry', industry)
+        record.set_field_value('Account_Type', account_type)
+        record.set_field_value('Campaign_Start_Date', campaign_start_date_api)  # note this format is different from the one I generated for the selenium input
+        record.set_field_value('Campaign_End_Date', campaign_end_date_api)  # note this format is different from the one I generated for the selenium input
+        record.set_field_value('Proposal_Date_Sent', proposal_date_api)  # note this format is different from the one I generated for the selenium input
+        record.set_field_value('Closing_Date', closing_date_api)  # note this format is different from the one I generated for the selenium input
+        record.set_field_value('Stage', stage)
+
+        contact_id = search_contact_by_name(sales_contact)  # uses my fn to grab the contact's ID
+        ac_dynamic_dict = {'name': sales_contact, 'id': contact_id}  # ... then pipes the ID and name into this dict for insertion into potential
+        # print('ac_dynamic_dict looks like this:')
+        # print(ac_dynamic_dict)
+        # print('Attempting to set Contact_Name using dynamic dict')
+        record.set_field_value('Contact_Name', ac_dynamic_dict)
+
+        resp = record.create()
+        new_potential_id = record.entity_id  # grabs the entity ID of the record, to then use this to look up the newly created potential
+        print(resp.status_code)
+        logging.debug(f"New potential's ID is {new_potential_id}")
+        return new_potential_id
+
+    except ZCRMException as ex:
+        print(ex.status_code)
+        print(ex.error_message)
+        print(ex.error_code)
+        print(ex.error_details)
+        print(ex.error_content)
+
+
+def get_potential_record_by_id(id):
+    try:
+        record = ZCRMRecord.get_instance('Potentials', id)
+        resp = record.get()
+        # print(resp.status_code)
+        # print(f"entity ID for this record is {resp.data.entity_id}")
+        # print(resp.data.created_by.id)
+        # print(resp.data.modified_by.id)
+        # print(resp.data.owner.id)
+        # print(resp.data.created_by.name)
+        # print(resp.data.created_time)
+        # print(resp.data.modified_time)
+        # print(resp.data.get_field_value('Email'))
+        # print(resp.data.get_field_value('Last_Name'))
+        # print('######### Attempting to display Full_Name - if this works I can return this value')
+        deal_name = resp.data.get_field_value('Deal_Name')
+        p_number = resp.data.get_field_value('Reference_Number')
+        # print(full_name)
+        print('Data for the potential looks like this:')
+        pprint(resp.data.field_data)
+        # if resp.data.line_items is not None:
+        #     for line_item in resp.data.line_items:
+        #         print("::::::LINE ITEM DETAILS::::::")
+        #         print(line_item.id)
+        #         print(line_item.product.get_field_value('Product_Code'))
+        return p_number
+
+    except ZCRMException as ex:
+        print(ex.status_code)
+        print(ex.error_message)
+        print(ex.error_code)
+        print(ex.error_details)
+        print(ex.error_content)
+
+
+# This is where the action is #####################################################################################
+# new_project_name = survey_name  # define what I want the name of the new potential to be
+# new_job_id = create_potential()  # create the new potential and store its ID in this variable
+# p_number = get_potential_record_by_id(new_job_id)  # use that ID to look up the newly created potential and store its P-number in this variable
+# print(f"p-number for new project is {p_number}")
 
 
 def login_sa():
@@ -334,18 +470,19 @@ def clean_up():
 
 
 # Variable Definition
-
+"""  # TURNING OFF FOR TESTING ##
 chrome_path = cfg.chrome_path  # location of chromedriver.exe on local drive
 chrome_options = Options()
 chrome_options.add_argument("--disable-notifications")  # to disable notifications popup in Chrome (affects Zoho page)
 driver = webdriver.Chrome(chrome_path, chrome_options=chrome_options)  # specify webdriver (chrome via selenium)
-
+"""
 # PULLING LEVERS HERE #############
 clean_up()
-login_zoho()
-enter_data_zoho()
-p_number = grab_p_number()
+# login_zoho()  # not needed in API mode
+# enter_data_zoho()  # not needed in API mode
+# p_number = grab_p_number()  # not needed in API mode
 
+"""  # TURNING OFF FOR TESTING ##
 new_project_dir_path = cfg.projects_dir_path + "\\" + client_name + "\\" + p_number + " - " + survey_name
 redirects_wb_path_name_ext = new_project_dir_path + "\\" + p_number + " redirects.xlsx"
 
@@ -357,5 +494,5 @@ subprocess.Popen(f'explorer "{new_project_dir_path}"')  # opens new dir in windo
 subprocess.Popen(f'explorer "{redirects_wb_path_name_ext}"')  # opens file in windows  # DISABLE FOR TESTING
 subprocess.Popen(f'explorer "{excel_file_name_path_ext}"')  # opens Survey Tracking file in windows, so I can add in project number manually  # DISABLE FOR TESTING
 
+"""
 
-# TODO: update Zoho section so it uses API instead of GUI
